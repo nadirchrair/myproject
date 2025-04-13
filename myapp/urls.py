cc90ec04f49ecf26177392f439a6566e979b8efb
nadir chrair <chrairmohamednadir@gmail.com>
2025-04-13 13:46:59 +0000

    push modification with logique

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('teams/', views.teams, name='teams'),
-    path('groupebactirie/', views.groupebactirie, name='groupebactirie'),
-    path('bactirie/', views.bactirie, name='bactirie'),
+    path('groupebactirie/<int:id>/', views.groupebactirie, name='groupebactirie'),
+    path('group/<int:group_id>/bacteries/', views.bactirie, name='bactirie'),
     path('bactirie_detail/', views.bactirie_detail, name='bactirie_detail'),
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from .models import Service, GroupBacterie, Bacterie
 
 def index(request):
-    return render(request, 'index.html')
+    services = Service.objects.all()
+    return render(request, 'index.html',{'services': services})
 
 def teams(request):
     return render(request, 'teams.html')
-def groupebactirie(request):
-    return render(request, 'groupebacterie.html')
-def bactirie(request):
-    return render(request, 'bactirie.html')
+def groupebactirie(request,id):
+    service = get_object_or_404(Service, id=id)
+    groups = service.groups.all()
+    return render(request, 'groupebacterie.html', {'service': service, 'groups': groups})
+def bactirie(request,group_id):
+    group = get_object_or_404(GroupBacterie, id=group_id)
+    bacteries = group.bacteries.all()
+    return render(request, 'bactirie.html',{'group': group, 'bacteries': bacteries})
 def bactirie_detail(request):
     return render(request, 'detials.html')
