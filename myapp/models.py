e72add7436f5bb7496d1371b8acadf1cd9b2fd01
nadir chrair <chrairmohamednadir@gmail.com>
2025-06-13 18:13:19 +0100

    push

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
-
+    image = models.ImageField(upload_to='service_images/', blank=True, null=True)
     def __str__(self):
         return self.name
 
